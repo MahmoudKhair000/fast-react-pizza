3e4e7758798e600c6c3c985cd150a350a6f66d78
MahmoudKh <mahmoudkhair000@outlook.com>
2026-07-27 20:25:48 +0300

    Refactor: Standardize import statements and update route paths to use single quotes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { createBrowserRouter } from "react-router";
-import { RouterProvider } from "react-router/dom";
+import { createBrowserRouter } from 'react-router';
+import { RouterProvider } from 'react-router/dom';
 
-import Error from "./ui/Error";
-import Home from "./ui/Home";
-import Menu, { loader as menuLoader } from "./features/menu/Menu";
-import Cart from "./features/cart/Cart";
-import Order, { loader as orderLoader } from "./features/order/Order";
+import Error from './ui/Error';
+import Home from './ui/Home';
+import Menu, { loader as menuLoader } from './features/menu/Menu';
+import Cart from './features/cart/Cart';
+import Order, { loader as orderLoader } from './features/order/Order';
 import CreateOrder, {
   action as createOrderAction,
-} from "./features/order/CreateOrder";
-import { action as updateOrderAction } from "./features/order/UpdateOrder";
-import AppLayout from "./ui/AppLayout";
+} from './features/order/CreateOrder';
+// import { action as updateOrderAction } from "./features/order/UpdateOrder";
+import AppLayout from './ui/AppLayout';
 
 /* 
   Using react-router v6.4+ data router APIs: createBrowserRouter accepts a tree
@@ -42,28 +42,28 @@ const router = createBrowserRouter([
       // Define the routes for the main content area
       // These routes will be rendered inside the Outlet in AppLayout
       // using <Outlet /> component
-      { path: "/", index: true, element: <Home /> },
+      { path: '/', index: true, element: <Home /> },
       // Define the imported loader for the menu route,
       // then use it in the menu component with 'useLoaderData()'
       // with no arguments, because it's already defined in the route.
       {
-        path: "/menu",
+        path: '/menu',
         element: <Menu />,
         loader: menuLoader,
         errorElement: <Error />,
       },
-      { path: "/cart", element: <Cart /> },
+      { path: '/cart', element: <Cart /> },
       {
-        path: "/order/new",
+        path: '/order/new',
         element: <CreateOrder />,
         errorElement: <Error />,
         action: createOrderAction,
       },
       {
-        path: "/order/:orderId",
+        path: '/order/:orderId',
         element: <Order />,
         loader: orderLoader,
-        action: updateOrderAction,
+        // action: updateOrderAction,
         errorElement: <Error />,
       },
     ],
